4173cccf0cc7be890de6a725ccaf86cb51e33415
Arnav-Naive <arnavfating09@gmail.com>
2026-09-03 11:31:36 +0530

    from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .models import Movie
 from .serializers import MovieSerializer
